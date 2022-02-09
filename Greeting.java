3fa3bdeb2f03a2ec8c2d16d14bdeec1a1062c50b
sourav <Sourav>
2022-02-09 22:00:53 +0530

    Add method is create

--- Greeting.java
+++ Greeting.java
@@ -2,5 +2,9 @@ class Greeting{
 	public Static void main(String[] args){
 		System.out.println("Welcome!");
 		System.out.println("My Name Is Sourav");
+		int a =5;
+		int b= 6;
+		int sum = a+b;
+		System.out.println(sum);
 	}
 }
